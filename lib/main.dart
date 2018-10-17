fb952198f098bb48db8cabe8b0a0fbbaa97e0067
suhas4090 <35102923+suhas4090@users.noreply.github.com>
2018-10-17 16:23:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:streambuilder/models/catalog.dart';
+import 'package:streambuilder/widgets/cart_button.dart';
+import 'package:streambuilder/widgets/product_square.dart';
+import 'package:streambuilder/widgets/theme.dart';
+import 'bloc_cart_page.dart';
+import 'cart_bloc.dart';
+import 'cart_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CartProvider(
+      child: MaterialApp(
+        title: 'Bloc',
+        theme: appTheme,
+        home: MyHomePage(),
+        routes: <String, WidgetBuilder>{
+          BlocCartPage.routeName: (context) => BlocCartPage()
+        },
+      ),
+    );
+  }
+}
+
+/// The sample app's main page
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cartBloc = CartProvider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bloc"),
+        actions: <Widget>[
+          StreamBuilder<int>(
+            stream: cartBloc.itemCount,
+            initialData: 0,
+            builder: (context, snapshot) => CartButton(
+                  itemCount: snapshot.data,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(BlocCartPage.routeName);
+                  },
+                ),
+          )
+        ],
+      ),
+      body: ProductGrid(),
+    );
+  }
+}
+
+/// Displays a tappable grid of products
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cartBloc = CartProvider.of(context);
+    return GridView.count(
+      crossAxisCount: 2,
+      children: catalog.products.map((product) {
+        return ProductSquare(
+          product: product,
+          onTap: () {
+            cartBloc.cartAddition.add(CartAddition(product));
+          },
+        );
+      }).toList(),
+    );
+  }
+}
